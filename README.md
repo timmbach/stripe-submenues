91fd30f42d61fded1f109fbfcb8cd5f47929d98c
TimmBach <follow.timothy@gmail.com>
2021-05-21 23:09:43 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Stripe homepage clone showing their special submenues
+
+## What I learned from this build
+
+- How to add hover functionality to reveal sublinks nested in navbar links when the user hovers through it
+- Improved my use of react context API
+- "getBoundingClientRect" function
---
+++ src/Navbar.js
@@ -0,0 +1,48 @@
+import React from "react";
+import logo from "./images/logo.svg";
+import { useGlobalContext } from "./context";
+import { FaBars } from "react-icons/fa";
+
+const Navbar = () => {
+  const { openSidebar, openSubmenu, closeSubmenu } = useGlobalContext();
+  const displaySubmenu = (e) => {
+    const page = e.target.textContent;
+    const tempBtn = e.target.getBoundingClientRect();
+    const center = (tempBtn.left + tempBtn.right) / 2;
+    const bottom = tempBtn.bottom - 3;
+
+    openSubmenu(page, { center, bottom });
+  };
+  return (
+    <nav className="nav">
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} className="nav-logo" alt="stripe" />
+          <button className="btn toggle-btn" onClick={openSidebar}>
+            <FaBars />
+          </button>
+        </div>
+        <ul className="nav-links">
+          <li>
+            <button className="link-btn" onMouseOver={displaySubmenu}>
+              products
+            </button>
+          </li>
+          <li>
+            <button className="link-btn" onMouseOver={displaySubmenu}>
+              developers
+            </button>
+          </li>
+          <li>
+            <button className="link-btn" onMouseOver={displaySubmenu}>
+              company
+            </button>
+          </li>
+        </ul>
+        <button className="btn signin-btn">sign in</button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
